3de12a65a24d7368dd5b9765fb449d4a716e3654
LeTung <mac@LTMacbook.local>
2024-03-13 16:49:25 +0700

    Optimized input, work like a charm! Conratulations to me!

--- ticktactoe.py
+++ ticktactoe.py
@@ -1,5 +1,6 @@
 # define print board to terminal every move
 def print_board(board):
+    print("-" * 9)
     for row in board:
         print(" | ".join(row))
         print("-" * 9)
@@ -26,7 +27,7 @@ def check_winner(board):
 
 def tick_tac_toe():
     board = [[' ' for _ in range(3)] for _ in range(3)]
-    current_player = input('Pick first move (X or O): ').upper()
+    current_player = input('Chose first player (x / o): ').upper()
     move_count = 0
     winner = ''
     print_board(board)
@@ -34,32 +35,35 @@ def tick_tac_toe():
     current_move = 'invalid'
 
     while winner =='':
-        print('Current player {current_player} turn:'.format(current_player = current_player))
+        print(f'Current player {current_player} turn:')
         while current_move == 'invalid':
-            row = int(input('Pick row (0, 1, 2): '))
-            col = int(input('Pick column (0, 1, 2): '))
-            if row > 2 or col > 2:
-                print('Input is out of range. Retry other')
-                continue
-            elif board[row][col] != ' ':
-                print('Position is taken. Retry other')
-                continue
-            else:
-                move_count += 1
-                print(f'{move_count} moved: {current_player} row {row}, column {col}')
-                # print('{move_count} moved: {current_player} row {row}, column {col}'.format(current_player = current_player, row = row, col = col, move_count= move_count))
-                current_move = 'valid'
+            try:
+                row = int(input('Pick row (0, 1, 2): '))
+                col = int(input('Pick column (0, 1, 2): '))
+                if row < 0 or row > 2 or col < 0 or col > 2:
+                    print('Input is out of range. Retry other')
+                elif board[row][col] != ' ':
+                    print('Position is taken. Retry other')
+                    continue
+                else:
+                    move_count += 1
+                    print('****************')
+                    print(f'{move_count} moved: {current_player} row {row}, column {col}')
+                    current_move = 'valid'
+            except ValueError:
+                print('Please input a Number')
         board[row][col] = current_player
         print_board(board)
+        print('****************')
 
         # check winner
         winner = check_winner(board)
         if winner != '':
-            print(f'{current_player} won, congratulation!')
+            print(f'Player {current_player} won, congratulation!')
             break
    
         # check if Tie
-        if move_count > 9:
+        if move_count >= 9:
             print('It\' a tie!')
             break
         # reset conditions
